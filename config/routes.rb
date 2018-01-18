755d94db97daa00921925f21e585bf3d1cc9f635
Frank Lam <frank.is.missing@gmail.com>
2018-01-18 05:56:19 +0000

    Implemented user authentication login/logout feature

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,12 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
 
+  resources :users, only: [:index, :new, :create]
+  
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   namespace :admin do
     root to: 'dashboard#show'
     resources :categories, :products, except: [:edit, :update, :show]
