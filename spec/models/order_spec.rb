2d1400cd38aa517ab8eb634e7041210dca524150
Frank Lam <frank.is.missing@gmail.com>
2018-01-21 21:09:38 +0000

    Completed feature-spec setup

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -13,29 +13,21 @@ RSpec.describe Order, type: :model do
     end
     # pending test 1
     it 'deducts quantity from products based on their line item quantities' do
-      # TODO: Implement based on hints below
       # 1. initialize order with necessary fields (see orders_controllers, schema and model definition for what is required)
       @order = Order.new(email: "test@test.com", total_cents: 999, stripe_charge_id: 10)
       # 2. build line items on @order
-      quantity = 2
       @order.line_items.new(
         product: @product1,
-        quantity: quantity,
+        quantity: 2,
         item_price: @product1.price,
         total_price: @product1.price * 20
       )
-      @product1.quantity -= quantity
-      @product1.save!
-
-      quantity = 10
       @order.line_items.new(
         product: @product2,
-        quantity: quantity,
+        quantity: 10,
         item_price: @product1.price,
         total_price: @product1.price * 20
       )
-      @product2.quantity -= quantity
-      @product2.save!
       # 3. save! the order - ie raise an exception if it fails (not expected)
       @order.save!
       # 4. reload products to have their updated quantities
@@ -44,11 +36,8 @@ RSpec.describe Order, type: :model do
       # 5. use RSpec expect syntax to assert their new quantity values
       expect(@product1.quantity).to eql(4998)
       expect(@product2.quantity).to eql(10)
-      # ...
     end
-    # pending test 2
     it 'does not deduct quantity from products that are not in the order' do
-      # TODO: Implement based on hints in previous test
       expect(@product3.quantity).to eql(90)
     end
   end
